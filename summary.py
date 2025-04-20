81f400b8b5a41d1abfa3f19bfe005a2fcedc9bec
omar ehab <omar.ehab.fawzy04@gmail.com>
2025-04-20 22:12:50 +0200

    Delete summary.py

--- summary.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
-from sumy.summarizers.luhn import LuhnSummarizer
-from sumy.summarizers.lex_rank import LexRankSummarizer
-from sumy.summarizers.text_rank import TextRankSummarizer
-
-def summarize(text,summarizer_type="lsa")

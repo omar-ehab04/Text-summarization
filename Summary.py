16b98cc3efe20173854a230b46cc46863807e774
omar ehab <omar.ehab.fawzy04@gmail.com>
2025-04-20 22:24:18 +0200

    Add files via upload

---
+++ Summary.py
@@ -0,0 +1,38 @@
+#pip install sumy 
+
+#pip uninstall nltk 
+
+#pip install nltk 
+import streamlit as st 
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
+
+def summarize(text,summarizer_type="lsa",sentence_count=5):
+    parser=PlaintextParser.from_string(text,Tokenizer("arabic"))
+    if summarizer_type=="lsa":
+        summarizer=LsaSummarizer()
+    elif summarizer_type=="luhn":
+        summarizer=LuhnSummarizer()
+    elif summarizer_type=="lexrank":
+        summarizer=LexRankSummarizer()
+    elif summarizer_type=="textrank":
+        summarizer=TextRankSummarizer()
+    else:
+        st.error("Please enter a valid name for summarizer")
+    summary=summarizer(parser.document,sentence_count)
+    return " ".join(str(sentence) for sentence in summary)
+
+st.title("Text Summarization")
+text=st.text_area("Please enter some text")
+summarizer_type=st.selectbox("Choose Summarizer",("lsa","luhn","lexrank","textrank"))
+sentence_count=st.slider("Number of Sentences",1,10,4)
+
+if st.button("Summarize"):
+    if text:
+        summary=summarize(text,summarizer_type,sentence_count)
+        st.subheader("Summary")
+        st.write(summary)
